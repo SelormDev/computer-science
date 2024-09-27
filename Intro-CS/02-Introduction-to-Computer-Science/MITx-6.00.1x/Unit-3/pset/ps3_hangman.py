21cf94d2c92d9a02dabf37dafe22176eab71db47
Gideon Selorm Ayivi <88237474+SelormDev@users.noreply.github.com>
2024-09-27 03:02:10 +0000

    Solve getGuessedWord function

--- Intro-CS/02-Introduction-to-Computer-Science/MITx-6.00.1x/Unit-3/pset/ps3_hangman.py
+++ Intro-CS/02-Introduction-to-Computer-Science/MITx-6.00.1x/Unit-3/pset/ps3_hangman.py
@@ -57,20 +57,29 @@ def isWordGuessed(secretWord, lettersGuessed):
     return True
 
 
+# secretWord = 'apple' 
+# lettersGuessed = ['p', 'a', 'l']
+# print(isWordGuessed(secretWord, lettersGuessed))
+
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    # FILL IN YOUR CODE HERE...
+    currentGuess = ''
+    for letter in secretWord:
+        if letter in lettersGuessed:
+            currentGuess += letter
+        else:
+            currentGuess += "_ "
+    return currentGuess
+
 secretWord = 'apple' 
-lettersGuessed = ['p', 'a', 'l']
-print(isWordGuessed(secretWord, lettersGuessed))
-
-# def getGuessedWord(secretWord, lettersGuessed):
-#     '''
-#     secretWord: string, the word the user is guessing
-#     lettersGuessed: list, what letters have been guessed so far
-#     returns: string, comprised of letters and underscores that represents
-#       what letters in secretWord have been guessed so far.
-#     '''
-#     # FILL IN YOUR CODE HERE...
-
-
+lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+print(getGuessedWord(secretWord, lettersGuessed))
 
 # def getAvailableLetters(lettersGuessed):
 #     '''
