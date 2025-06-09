46bd14fca8f3ecc13b73c0ae107b42338ee27672
SelormDev <88237474+SelormDev@users.noreply.github.com>
2025-06-09 18:30:23 +0000

    complete signature, purpose and stub

---
+++ Core-CS/Systematic-Program-Design/Part-01/1b-HtDF/practice-problems/less-than-five.rkt
@@ -0,0 +1,16 @@
+#lang htdp/bsl
+
+;PROBLEM:
+;
+;DESIGN function that consumes a string and determines whether its length is
+;less than 5.  Follow the HtDF recipe and leave behind commented out versions 
+;of the stub and template.
+
+;; Solution:
+
+; String -> Boolean
+; Determine whether the length of a string is less than 5
+
+(define (check-length s) 0)          ;stub
+
+(check-length "hi")
