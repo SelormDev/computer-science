0d5995e530b5e7bbd490020dc155371250ac3287
SelormDev <88237474+SelormDev@users.noreply.github.com>
2025-06-09 18:29:17 +0000

    rename file

--- Core-CS/Systematic-Program-Design/Part-01/1b-HtDF/practice-problems/less-than-five-starter.rkt
+++
@@ -1,7 +0,0 @@
-#lang htdp/bsl
-
-;PROBLEM:
-;
-;DESIGN function that consumes a string and determines whether its length is
-;less than 5.  Follow the HtDF recipe and leave behind commented out versions 
-;of the stub and template.
