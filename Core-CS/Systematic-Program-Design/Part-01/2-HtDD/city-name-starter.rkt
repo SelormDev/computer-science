9e12556f6e4ee8d122f06e0b03003cfb5771d66b
SelormDev <88237474+SelormDev@users.noreply.github.com>
2025-07-06 10:07:47 +0000

    add city-name-starter.rkt file

---
+++ Core-CS/Systematic-Program-Design/Part-01/2-HtDD/city-name-starter.rkt
@@ -0,0 +1,10 @@
+#lang htdp/bsl
+
+
+; PROBLEM:
+
+; Imagine that you are designing a program that, among other things, 
+; has information about the names of cities in its problem domain.
+
+; Design a data definition to represent the name of a city. 
+   
